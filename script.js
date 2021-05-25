2b10f11adde39754359bd4cdc42ed53c530c1b52
artiom sokolov <asokolik87@gmail.com>
2021-05-25 22:31:05 +0300

    variables

---
+++ script.js
@@ -0,0 +1,11 @@
+let admin; 
+let name;
+
+name = "Jon";
+admin = name;
+alert (admin);  //Jon
+
+let ourPlanetName;
+ourPlanetName = "Earth"; //Создайте переменную для названия нашей планеты. Как бы вы её назвали?
+
+let currentUserName; //Создайте переменную для хранения имени текущего посетителя сайта. Как бы вы назвали такую переменную?
