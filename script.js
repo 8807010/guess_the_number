4d2d73f6125fd46917eba214468224f85a98e235
Artiom_Sokolov <sokolova-net@mail.ru>
2022-09-03 21:11:52 +0300

    add preview

--- script.js
+++ script.js
@@ -1,137 +1,27 @@
-// let admin; 
-// let name;
+const answer = Math.floor(Math.random() * 100) + 1;
+let attempts = 0;
 
-// name = "Jon";
-// admin = name;
-// alert (admin);  //Jon
+while (true) {
+    const userAnswer = prompt('Введите целое цисло от 1 до 100. Для выхода введите "выход"');
 
+    if (userAnswer.toLocaleLowerCase() === "выход") {
+        break;
+    }
 
-
-
-// let ourPlanetName;
-// ourPlanetName = "Earth"; //Создайте переменную для названия нашей планеты. Как бы вы её назвали?
-
-
-
-
-// let currentUserName; //Создайте переменную для хранения имени текущего посетителя сайта. Как бы вы назвали такую переменную?
+    const userNumber = Number.parseInt(userAnswer, 10);
+    if (Number.isNaN(userNumber)) {
+        alert('Вы ввели не число');
+        continue;
+    }
 
 
-
-
-// // Создайте страницу, которая спрашивает имя у пользователя и выводит его.
-
-// let name = prompt("Ваше имя?", "");
-// alert(`Hello, ${name}`);
-
-
-
-
-
-// Чему будут равны переменные a и x после исполнения кода в примере ниже?
-let a = 2;
-
-let x = 1 + (a *= 2);
-
-// Ответ:
-// a = 4 (умножено на 2)
-// x = 5 (вычислено как 1 + 4)
-
-
-
-
-// Какой результат будет у выражений ниже?
-
-// "" + 1 + 0 = "10"
-// "" - 1 + 0 = -1 
-// true + false = 1
-// 6 / "3" = 2
-// "2" * "3" = 6
-// 4 + 5 + "px" = "9px"
-// "$" + 4 + 5 = "$45"
-// "4" - 2 = 2
-// "4px" - 2 = NaN
-// 7 / 0 = Infinity
-// "  -9  " + 5 = "  -9  5"
-// "  -9  " - 5 = -14
-// null + 1 = 1 
-// undefined + 1 = NaN
-// " \t \n" - 2 = -2 
-
-
-
-
-
-// Замените код Function Expression стрелочной функцией:
-
-
-// function ask(question, yes, no) {
-//     if (confirm(question)) yes()
-//     else no();
-//   }
-  
-//   ask(
-//     "Вы согласны?",
-//     function() { alert("Вы согласились."); },
-//     function() { alert("Вы отменили выполнение."); }
-//   );
-
-
-
-//   function ask(question, yes, no) {
-//     if (confirm(question)) yes()
-//     else no();
-//   }
-  
-//   ask(
-//     "Вы согласны?",
-//     () => alert("Вы согласились."),
-//     () => alert("Вы отменили выполнение.")
-//   );
-
-
-
-
-
-
-// Напишите код, выполнив задание из каждого пункта отдельной строкой:
-
-// Создайте пустой объект user.
-// Добавьте свойство name со значением John.
-// Добавьте свойство surname со значением Smith.
-// Измените значение свойства name на Pete.
-// Удалите свойство name из объекта.
-
-// let user = {};
-// user.name = "John";
-// user.surname = "Smith";
-// user.name = "Pete";
-// delete user.name;
-
-
-
-
-// У нас есть объект, в котором хранятся зарплаты нашей команды:
-
-// let salaries = {
-//   John: 100,
-//   Ann: 160,
-//   Pete: 130
-// }
-// Напишите код для суммирования всех зарплат и сохраните результат в переменной sum. Должно получиться 390.
-
-// Если объект salaries пуст, то результат должен быть 0.
-
-
-// let salaries = {
-//   John: 100,
-//   Ann: 160,
-//   Pete: 130
-// }
-
-// let sum = 0;
-// for (let key in salaries) {
-//   sum += salaries[key];
-// }
-
-// alert (sum);
+    attempts++;
+    if (userNumber > answer) {
+        alert('Попробуйте звять число меньше');
+    } else if (userNumber < answer) {
+        alert('Попробуйте взять число больше');
+    } else {
+        alert('Верно! Число отгадано с ' + attempts + ' попытки!');
+        break;
+    }
+}
