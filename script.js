2a0fe59f1d270daaae97af310bf312446ec19f67
Artiom_Sokolov <sokolova-net@mail.ru>
2022-09-03 21:19:58 +0300

    change name repository

--- script.js
+++ script.js
@@ -2,26 +2,26 @@ const answer = Math.floor(Math.random() * 100) + 1;
 let attempts = 0;
 
 while (true) {
-    const userAnswer = prompt('Введите целое цисло от 1 до 100. Для выхода введите "выход"');
+  const userAnswer = prompt('Введите целое цисло от 1 до 100. Для выхода введите "выход"');
 
-    if (userAnswer.toLocaleLowerCase() === "выход") {
-        break;
-    }
+  if (userAnswer.toLocaleLowerCase() === "выход") {
+    break;
+  }
 
-    const userNumber = Number.parseInt(userAnswer, 10);
-    if (Number.isNaN(userNumber)) {
-        alert('Вы ввели не число');
-        continue;
-    }
+  const userNumber = Number.parseInt(userAnswer, 10);
+  if (Number.isNaN(userNumber)) {
+    alert('Вы ввели не число');
+    continue;
+  }
 
 
-    attempts++;
-    if (userNumber > answer) {
-        alert('Попробуйте звять число меньше');
-    } else if (userNumber < answer) {
-        alert('Попробуйте взять число больше');
-    } else {
-        alert('Верно! Число отгадано с ' + attempts + ' попытки!');
-        break;
-    }
+  attempts++;
+  if (userNumber > answer) {
+    alert('Попробуйте звять число меньше');
+  } else if (userNumber < answer) {
+    alert('Попробуйте взять число больше');
+  } else {
+    alert('Верно! Число отгадано с ' + attempts + ' попытки!');
+    break;
+  }
 }
